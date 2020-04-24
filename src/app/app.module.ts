d77d1c4d5600314f09e1ba3ff379b0789ce6d5ff
KevinW <bird831205@hotmail.com>
2020-04-24 16:27:41 -0400

    bootstrap and some boilerplate code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TodosComponent } from './todos/todos.component';
+import { ZippyComponent } from './zippy/zippy.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodosComponent,
+    ZippyComponent
   ],
   imports: [
     BrowserAnimationsModule,
