5ef749fab6370b8f844e8c369657c4ae00b10c4f
KevinW <bird831205@hotmail.com>
2020-04-24 22:18:34 -0400

    refactoring

--- src/app/zippy/zippy.component.ts
+++ src/app/zippy/zippy.component.ts
@@ -1,39 +1,13 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { trigger, state, style, transition, animate } from '@angular/animations';
+import { expandCollapse } from './zippy.component.animation';
 
 @Component({
   selector: 'app-zippy',
   templateUrl: './zippy.component.html',
   styleUrls: ['./zippy.component.css'],
   animations: [
-    trigger("expandCollapse", [
-      state('collapsed', style({
-        height: 0,
-        overflow: "hidden",
-        paddingTop: 0,
-        paddingBottom: 0,
-        opacity: 0
-      })),
-      state('expanded', style({
-        height: '*',
-        padding: '*',
-        overflow: 'auto'
-      })),
-      transition('collapsed => expanded', [
-        animate('300ms ease-out', style({
-          height: '*',
-          paddingTop: '*',
-          paddingBottom: '*'
-        })),
-        animate('1s', style({
-          opacity: 1
-        }))
-      ]),
-
-      transition('expanded => collapsed', [
-        animate('300ms ease-in')
-      ])
-    ])
+    expandCollapse
   ]
 })
 export class ZippyComponent {
